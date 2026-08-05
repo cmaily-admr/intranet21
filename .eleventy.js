0daed6f932c371775536be66fc173cb2778571ef
cmaily-admr <cmaily@fede21.admr.org>
2026-08-05 13:27:22 +0200

    Implement .md data extension using gray-matter
    Added support for reading .md files in _data/ directory.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const matter = require("gray-matter");
+
 module.exports = function (eleventyConfig) {
   // Le back-office Decap : source dans public/admin, publié sur /admin
   eleventyConfig.addPassthroughCopy({ "public/admin": "admin" });
@@ -8,6 +10,12 @@ module.exports = function (eleventyConfig) {
 
   // Redéclenche le build quand un document est ajouté
   eleventyConfig.addWatchTarget("./uploads/");
+
+  // Permet de lire les fichiers .md placés dans _data/
+  eleventyConfig.addDataExtension("md", (contents) => {
+    const parsed = matter(contents);
+    return { ...parsed.data, body: parsed.content };
+  });
 
   // Filtre pour convertir objet en array
   eleventyConfig.addFilter("values", (value) => {
